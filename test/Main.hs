62e5038679550079be92fb30c1de4daee7c196d7
Harendra Kumar <harendra.kumar@gmail.com>
2017-02-12 06:34:06 +0530

    Add sandbox based test cases

--- test/Main.hs
+++ test/Main.hs
@@ -7,12 +7,19 @@ import Pipes
 import Pipes.Files
 import Pipes.Prelude (toListM)
 import Pipes.Tree
+import System.Directory (createDirectoryIfMissing)
+import System.FilePath ((</>))
+import System.IO.Temp (withSystemTempDirectory)
 import Test.Hspec
 
 main :: IO ()
-main = hspec $ do
-    describe "Sanity tests" $ do
-        it "Finds expected files in project" findsExpected
+main = do
+    hspec $ do
+        describe "Sanity tests" $ do
+            it "Finds expected files in project" findsExpected
+    hspec . around withSandbox $ do
+        beforeWith populateDirTree $ do
+            describe "listDirTree" listDirTreeSpec
 
 findsExpected :: Expectation
 findsExpected = do
@@ -46,3 +53,42 @@ findsExpected = do
                    ]
     found <- toListM $ enumerate (walk files)
     sort found `shouldBe` expected
+
+withSandbox :: ActionWith FilePath -> IO ()
+withSandbox = withSystemTempDirectory "pipes-files-test-sandbox"
+
+-- Create a directory tree in a sandbox and test if we can list it
+-- correctly. keep it sorted.
+
+dirTree :: ([FilePath], [FilePath])
+dirTree =
+  (
+      [ "a"
+      , "b"
+      , "b/c"
+      ],
+      [
+        "b/c/three.txt"
+      , "b/two.txt"
+      , "one.txt"
+      ]
+  )
+
+populateDirTree :: FilePath -> IO FilePath
+populateDirTree top = do
+  let (dirs, files) = dirTree
+      pdir          = top </> "pdir"
+      withinSandbox = (pdir </>)
+  createDirectoryIfMissing True pdir
+  forM_ dirs (createDirectoryIfMissing True . withinSandbox)
+  forM_ files $ (`writeFile` "") . withinSandbox
+  return pdir
+
+listDirTree :: FilePath -> IO [FilePath]
+listDirTree dir = toListM (enumerate (walk files)) >>= return . sort
+    where files = directoryFiles dir
+
+listDirTreeSpec :: SpecWith FilePath
+listDirTreeSpec = it "lists directory tree" $ \dir ->
+    listDirTree dir `shouldReturn` map (dir </>) (d ++ f)
+        where (d, f) = dirTree
